abe4a1be4a265f129860431458f2f67d94219007
renatossanches <renato.sanches2004@gmail.com>
2025-06-05 11:40:52 -0300

    Feature - Creation function Global getPayload

---
+++ src/components/contexts/UserContextGlobal.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { GetReturnPayload } from "../../routers/private/requests/get/GetReturnPayload";
+
+const UserContext = createContext();
+
+export function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+
+    const getUser = () => user;
+
+    useEffect(() => {
+        async function fetchUser() {
+            const fetchedUser = await GetReturnPayload();
+            setUser(fetchedUser);
+        }
+        fetchUser();
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ user, setUser, getUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+
+export function useUser() {
+    return useContext(UserContext);
+}
